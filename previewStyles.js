c0588137023a42417f6d1cb81e6dda74eb6c8efc
Adam Collier <adamcollierart@gmail.com>
2018-08-10 11:24:31 +0100

    console log steps to see what happens in deploy

--- previewStyles.js
+++ previewStyles.js
@@ -3,15 +3,21 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 fs.readdir('./docs/.vuepress/dist/assets/css', (err, files) => {
+    if (err) console.log(err);
+
     if (files.length) {
         let cssScript = `<script>CMS.registerPreviewStyle("/assets/css/${files[0]}")</script>`;
         let adminPath = './docs/.vuepress/public/admin/index.html'
         let configIndex = fs.readFileSync(adminPath, 'utf-8')
 
+        console.log(configIndex);
+
         var { document } = new JSDOM(configIndex).window;
         document.querySelector('body').insertAdjacentHTML("beforeend", cssScript);
 
         let adminHTML = document.documentElement.outerHTML;
+
+        console.log(adminHTML);
 
         fs.writeFileSync(adminPath, adminHTML, "utf-8");
 
